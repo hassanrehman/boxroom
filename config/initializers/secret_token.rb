6b031bb48ed319436df1ae42b9e3338ec9536fa7
hassan abdul rehman <hassan.rehman@gmail.com>
2014-03-26 12:36:46 +0500

    msie bug fixed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,2 @@
+puts "setting key"
+Boxroom::Application.config.secret_key_base = 'e82fd5af1420f3c7d5dff4e6e76c173a4db77477ec331b5606a62b7cfec3f7760918fbd9e521275d222716ed9ee1a1641f2f013e09325c304232e309292298cc'
